0a50870b7cf05f1a91db16e5b58549403fabd274
josue martinez <josuemr_02@hotmail.com>
2023-03-13 19:31:31 -0700

    add layout

---
+++ card.js
@@ -0,0 +1,38 @@
+
+// analizar documento
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.all);
+// console.log(document.all[6])
+
+// GETELEMENTBYID
+
+// var myH1 = document.getElementById("pop");
+// console.log(myH1);
+// myH1.innerHTML = "el mensaje favorito";
+// myH1.innerText = "cambio de mensaje";
+// myH1.style.color = "blue";
+
+
+// GETELEMENTBYCLASSNAME
+
+// var pet = document.getElementsByTagName("h1");
+// pet[0].style.color = "blue";
+// console.log(pet[0]);
+
+// QUERYSELECTOR
+
+// var query = document.querySelector(".ninja");
+// query.style.border = "4px solid"
+// console.log(query);
+
+var parent = document.querySelector("h1");
+// parent.parentElement.style.backgroundColor = "red";
+// console.log(parent.parentElement);
+
+// console.log(parent.childNodes);
+console.log(parent.children)
+
+
+
+
